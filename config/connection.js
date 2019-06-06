5756f1e2c76af5ffc3afbae1cb1ae1a55781dbd5
Waqar Habib <waqarhabib94@aggienetwork.com>
2019-06-05 20:40:55 -0500

    updated connection.js with JAWSDB info

--- config/connection.js
+++ config/connection.js
@@ -6,13 +6,20 @@
 var mysql = require("mysql");
 
 // Set up our connection information
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "bootcamp",
-  database: "tacoya_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "bootcamp",
+        database: "tacoya_db"
+      });
+};
+
 
 // Connect to the database
 connection.connect(function(err) {
